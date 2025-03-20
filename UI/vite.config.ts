39d46428a8d2201d965b69919c2d434910652de0
BenjaminMiddleton <benjamin.middleton@mail.bcu.ac.uk>
2025-03-20 23:25:57 +0000

    Changed Deploy file

--- UI/vite.config.ts
+++ UI/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Meeting-Management-Project/', // Change this to match the repository name
-})
+  base: '/Craig_Nav/',  // This should match your repository name
+});
